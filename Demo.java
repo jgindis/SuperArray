5c9dc18e52c36eb19a55390605648e96a161a698
jgindis <jgindis20@stuy.edu>
2020-11-09 11:21:57 -0500

    Fixed methods

--- Demo.java
+++ Demo.java
@@ -1,11 +1,11 @@
 public class Demo {
 
   public static void removeDuplicates(SuperArray s) {
-    for (int i = 0; i < SuperArray.size; i++) {
-      String cur = data[i];
-      for (int j = i + 1; j < SuperArray.size-i; j++) {
-        if (data[j].equals(cur)) {
-          remove(j);
+    for (int i = 0; i < s.size(); i++) {
+      String cur = s.get(i);
+      for (int j = i + 1; j < s.size()-i; j++) {
+        if (s.get(j).equals(cur)) {
+          s.remove(j);
           i--;
         }
       }
@@ -13,36 +13,25 @@ public class Demo {
   }
 
   public static SuperArray findOverlap(SuperArray a, SuperArray b) {
-    int max = Math.max(a.size(),b.size());
-    int min = Math.min(a.size(),b.size());
-    SuperArray c = new SuperArray(min);
- 		for (int i = 0; i < max; i++) {
- 			if (a.size() > i && b.contains(a.get(i))) {
-        if (!(c.contains(a.get(i)))) c.add(a.get(i));
-      }
- 			if (b.size() > i && a.contains(b.get(i))) {
-        if (!(c.contains(b.get(i)))) c.add(b.get(i));
- 		  }
- 		return c;
+    SuperArray c = new SuperArray();
+    for (int i = 0; i < a.size(); i++) {
+      if (b.contains(a.get(i))) c.add(a.get(i));
     }
+    removeDuplicates(c);
+    return c;
   }
 
   public static SuperArray zip(SuperArray a, SuperArray b) {
     SuperArray c = new SuperArray();
-    if (a.size() >= b.size()) {
-      int max = a.size();
-      int min = b.size();
-    } else {
-      int max = b.size();
-      int min = a.size();
+    int max = Math.max(a.size(),b.size());
+    int min = Math.min(a.size(),b.size());
+    for (int i = 0; i < min; i++) {
+      c.add(a.get(i));
+      c.add(b.get(i));
     }
-      for (int i = 0; i < min; i++) {
-        c.add(a.get(i));
-        c.add(b.get(i));
-      }
-      for (int i = min; i < max; i++) {
-        c.add(a.get(i));
-      }
+    for (int i = min; i < max; i++) {
+      c.add(a.get(i));
+    }
     return c;
   }
 
